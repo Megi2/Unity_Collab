0a08ec58a23dbd58cc3fc0d237ee4dd77057b395
Megi2 <65165556+Megi2@users.noreply.github.com>
2023-04-12 03:49:22 +0900

    230412

--- Collab_230404/Assets/NPCTalk.cs
+++ Collab_230404/Assets/NPCTalk.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct TalkData
+{
+    public string name;
+    public string[] contexts;
+
+}
 public class NPCTalk : MonoBehaviour
 {
     public GameObject TalkPanel;
@@ -10,7 +16,6 @@ public class NPCTalk : MonoBehaviour
     {
         TalkPanel.SetActive(false);
         List<Dictionary<string, object>> Talk_Dialogue = CSVReader.Read("Dialogue");
-        Debug.Log(Talk_Dialogue[1]);
     }
 
     // Update is called once per frame
